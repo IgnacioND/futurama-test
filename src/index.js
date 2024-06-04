3f51d99a24f67e1534578e354eeb0309f4582a23
ignacioND <ignacio.navarro@gmail.com>
2024-06-04 19:47:55 +0200

    Create styles for card and list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { FavoritesProvider } from "./context/FavoritesContext";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
