817585b9327045408020c808549a76f78f3a4813
ignacioND <ignacio.navarro@gmail.com>
2024-06-04 19:42:32 +0200

    Add Home button and character list depending on viewport size

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,11 +1,24 @@
 import React from "react";
 import CharacterDetail from "../components/CharacterDetail";
+import CharacterList from "../components/CharacterList";
+import { useMediaQuery } from "react-responsive";
 
 const BASE_CLASS = "detail-page";
 
 const DetailPage = () => {
+  const isLargerThanMobile = useMediaQuery({ query: `(min-width: 768px)` });
   return (
     <div className={BASE_CLASS}>
+      {isLargerThanMobile ? (
+        <CharacterList />
+      ) : (
+        <button
+          className={`${BASE_CLASS}-button`}
+          onClick={() => (window.location.href = "/")}
+        >
+          Homepage
+        </button>
+      )}
       <CharacterDetail />
     </div>
   );
