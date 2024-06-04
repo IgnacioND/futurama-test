16b3107b729d7dd3c9e72d14fb8ef8363000f1b6
ignacioND <ignacio.navarro@gmail.com>
2024-06-04 19:23:38 +0200

    Create Detail page and add route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListPage from "./pages/ListPage";
+import DetailPage from "./pages/DetailPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ListPage />} />
+        <Route path="/character/:id" element={<DetailPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/DetailPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BASE_CLASS = "detail-page";
+
+const DetailPage = () => {
+  return <div className={BASE_CLASS}>Character Detail</div>;
+};
+
+export default DetailPage;
