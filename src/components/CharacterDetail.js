7dc1aaedc5d2a4b509b6fa9864fbdc449bb8cce3
ignacioND <ignacio.navarro@gmail.com>
2024-06-04 19:27:14 +0200

    Create detail component with similar structure

---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCharacterDetails } from "../api";
+
+const BASE_CLASS = "character-detail";
+
+const CharacterDetail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCharacterDetails(id);
+      setCharacter(data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [id]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={BASE_CLASS}>
+      <div className={`${BASE_CLASS}-content`}>
+        <img
+          src={character.images.main}
+          width={96}
+          alt={character.name.first}
+        />
+        <div className={`${BASE_CLASS}-info`}>
+          <h2>{`${character.name.first} ${character.name.last}`}</h2>
+          <p>{`${character.age} - ${character.gender} - ${character.species}`}</p>
+          <p>{`${character.occupation}`}</p>
+          <ul>
+            {character.sayings.map((saying, index) => (
+              <li key={index}>{saying}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterDetail;
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import CharacterDetail from "../components/CharacterDetail";
 
 const BASE_CLASS = "detail-page";
 
 const DetailPage = () => {
-  return <div className={BASE_CLASS}>Character Detail</div>;
+  return (
+    <div className={BASE_CLASS}>
+      <CharacterDetail />
+    </div>
+  );
 };
 
 export default DetailPage;
