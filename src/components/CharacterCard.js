7766b98f237f5a2c2860c4a4b89e51ea78c624c7
ignacioND <ignacio.navarro@gmail.com>
2024-06-04 19:21:22 +0200

    Add image for character

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BASE_CLASS = "character-card";
 
 const CharacterCard = ({ character }) => {
   return (
     <div className={BASE_CLASS}>
-      <div className={`${BASE_CLASS}-content`}>
-        <h2>{`${character.name.first} ${character.name.last}`}</h2>
-        <p>{`${character.age} - ${character.gender} - ${character.species}`}</p>
-        <p>{`${character.occupation}`}</p>
-      </div>
+      <Link to={`/character/${character.id}`}>
+        <div className={`${BASE_CLASS}-content`}>
+          <img
+            src={character.images.main}
+            width={48}
+            alt={character.name.first}
+          />
+          <div className={`${BASE_CLASS}-info`}>
+            <h2>{`${character.name.first} ${character.name.last}`}</h2>
+            <p>{`${character.age} - ${character.gender} - ${character.species}`}</p>
+            <p>{`${character.occupation}`}</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
